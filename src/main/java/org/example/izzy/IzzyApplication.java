9c5a7fae7ee6102edc601a1aca47001e04c05228
athimjon <tonym5one@gmail.com>
2025-07-11 10:08:25 +0500

    Initial commit

---
+++ src/main/java/org/example/izzy/IzzyApplication.java
@@ -0,0 +1,13 @@
+package org.example.izzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzzyApplication.class, args);
+    }
+
+}
